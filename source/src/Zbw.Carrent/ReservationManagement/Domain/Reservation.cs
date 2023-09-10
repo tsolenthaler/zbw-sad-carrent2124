8dd1a487ac4a1606c94a62b227ccc8bbe7e62f0e
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-09-10 06:54:36 +0200

    fix Reservation with TotalCost

--- source/src/Zbw.Carrent/ReservationManagement/Domain/Reservation.cs
+++ source/src/Zbw.Carrent/ReservationManagement/Domain/Reservation.cs
@@ -6,8 +6,10 @@ namespace Zbw.Carrent.ReservationManagement.Domain
     public class Reservation
     {
         public Guid Id { get; set; }
+        public string ReservationNr { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public decimal TotalCost { get; set; }
         public Guid CarId { get; set; }
         public Car Car { get; set; }
         public Guid CustomerId { get; set; }
