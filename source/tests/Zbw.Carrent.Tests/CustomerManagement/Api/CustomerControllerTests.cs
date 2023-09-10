7b93e7ed1ba20b2e964cd57d43e8ece2d585e338
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-09-10 07:16:13 +0200

    add CustomerTest

--- source/tests/Zbw.Carrent.Tests/CustomerManagement/Api/CustomerControllerTests.cs
+++ source/tests/Zbw.Carrent.Tests/CustomerManagement/Api/CustomerControllerTests.cs
@@ -7,8 +7,9 @@
     using System.Threading.Tasks;
 
     using FluentAssertions;
-
+    using Moq;
     using Zbw.Carrent.CustomerManagement.Api;
+    using Zbw.Carrent.CustomerManagement.Api.Models;
     using Zbw.Carrent.CustomerManagement.Domain;
 
     public class CustomerControllerTests
@@ -25,11 +26,17 @@
         public void Post_Should_CreateCustomer()
         {
             // Arrange
-        
-            // Act  
+            var customerid = Guid.NewGuid();
+            var customerRepositoryMock = new Mock<ICustomerRepository>();
+            var customerController = new CustomerController(customerRepositoryMock.Object);
+            var customerRequest = new CustomerRequest(customerid, "123", "Test", "Test", "Test");
+
+            // Act
+            var result = customerController.Post(customerRequest);
 
             // Assert
-           
+            result.Should().BeEquivalentTo(new CustomerResponse(customerid, "123", "Test", null));
+            //customerRepositoryMock.Verify(x => x.Add(It.Is<Customer>(c => c.Id == customerid && c.CustomerNr == "123" && c.Name == "Test")), Times.Once);
         }
     }
 
